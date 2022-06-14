591006aa8cd5ebaf05dd62629749a7884f8449f0
Vladimir Jovanovic <vladimirjo@protonmail.com>
2022-06-14 19:50:05 +0200

    change validation with use of ApiController

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -3,6 +3,7 @@ using AspNetWebApp.Models;
 
 namespace AspNetWebApp.Controllers;
 
+[ApiController]
 [Route("api/[controller]")]
 public class ProductsController : ControllerBase
 {
@@ -32,20 +33,16 @@ public class ProductsController : ControllerBase
 
     [HttpPost]
     public async Task<IActionResult> 
-        SaveProducts([FromBody] ProductBindingTarget target)
+        SaveProducts(ProductBindingTarget target)
     {
-        if (ModelState.IsValid)
-        {
-            Product p = target.ToProduct();
-            await context.Products.AddAsync(p);
-            await context.SaveChangesAsync();
-            return Ok(p);
-        }
-        return BadRequest(ModelState);
+        Product p = target.ToProduct();
+        await context.Products.AddAsync(p);
+        await context.SaveChangesAsync();
+        return Ok(p);
     }
 
     [HttpPut]
-    public async Task UpdateProduct([FromBody] Product product)
+    public async Task UpdateProduct(Product product)
     {
         context.Products.Update(product);
         await context.SaveChangesAsync();
